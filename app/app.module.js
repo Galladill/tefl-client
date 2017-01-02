0bbf6a704ccc745c78e2d4cb5b6b405b41c770cb
Jordan Caudill <jacaudill74@gmail.com>
2017-01-01 18:21:21 -0600

    First components

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,7 @@
 (function() {
-    var app = angular.module('app', []);
+    var app = angular.module('app', ['user', 'signup']);
     
     app.config(['$locationProvider', function($locationProvider) {
-        console.log($locationProvider);
-        $locationProvider.html5Mode(true);
-        $locationProvider.hashPrefix('');
     }]);
    
 })();

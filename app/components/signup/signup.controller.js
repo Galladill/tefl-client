01dfa655fd5299d932c5961c03af14afabfb0efc
Jordan Caudill <jacaudill74@gmail.com>
2017-01-06 15:09:08 -0600

    ferple

--- app/components/signup/signup.controller.js
+++ app/components/signup/signup.controller.js
@@ -10,52 +10,26 @@
     function SignupController($scope) {
         // Attach functions to the controller here.
 		var vm = this;
-		vm.loadData = _loadData;
-		vm.editData = _editData;
-		vm.deleteData = _deleteData;
-		vm.createData = _createData;
-		vm.saveData = _saveData;
+		// vm.loadData = _loadData;
 		
 		// Any logic that needs to run when the controller loads should be placed here.
-		vm.loadData();
+
+		// vm.loadData();
+		// vm.test = 'hey';
 
 		// Define functions here.
-		function _loadData(id){
-			$scope.getData = dataService.getData();
-			$scope.getData.then(
-					function(data){
-						$scope.data = data;
-					},
-					function(data, status){
-					}
-			);
-		};
+		
+		// function _loadData(id){
+		// 	$scope.getData = dataService.getData();
+		// 	$scope.getData.then(
+		// 			function(data){
+		// 				$scope.data = data;
+		// 			},
+		// 			function(data, status){
+		// 			}
+		// 	);
+		// };
 
-		function _editData(id) {
-			dataService.getData(id);
-			$location.path('/productdetail/' + id);
-		};
-
-		function _deleteData(id, name) {
-			dataService.deleteData(id, name);
-			$scope.Data = dataService.getData();
-			$route.reload();
-			$location.path('/products');
-		};
-
-		function _createData() {
-			$route.reload();
-			$location.path('/productcreate');
-		};
-
-		function _saveData() {
-			$scope.data = {};
-			$scope.data.name = $scope.name;
-			$scope.data.description = $scope.description;
-			dataService.createData($scope.data);
-			$location.path('/products');
-			$route.reload();
-		};
 
     }
 })();
--- app/components/user/user.controller.js
+++ app/components/user/user.controller.js
@@ -8,6 +8,28 @@
     UserController.$inject = ['$scope'];
 
     function UserController($scope) {
-        
+        // Attach functions to the controller here.
+		var vm = this;
+		// vm.loadData = _loadData;
+		
+		// Any logic that needs to run when the controller loads should be placed here.
+
+		// vm.loadData();
+		// vm.test = 'hey';
+
+		// Define functions here.
+		
+		// function _loadData(id){
+		// 	$scope.getData = dataService.getData();
+		// 	$scope.getData.then(
+		// 			function(data){
+		// 				$scope.data = data;
+		// 			},
+		// 			function(data, status){
+		// 			}
+		// 	);
+		// };
+
+
     }
 })();

cd0b16507970e65c673fd5bb590d17471348c7a0
Jordan Caudill <jacaudill74@gmail.com>
2017-01-06 11:45:12 -0600

    test setup

--- test/signup/signup.controller.spec.js
+++ test/signup/signup.controller.spec.js
@@ -1,15 +1,12 @@
 var chai = require('chai');
 var should = chai.should();
 
-console.log('awsdgfgasasdasddasdasdhgc');
 describe("A suite is just a function", function () {
-    console.log('asghbngsfsefd');
     var a;
 
     it("and so is a spec", function () {
         a = true;
-        console.log('wtf');
 
-        expect(a).toBe(true);
+        a.should.equal(true);
     });
 });

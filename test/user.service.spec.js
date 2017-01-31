4c5efc582508390533d41e57a3bdaa60b8cdb7ed
Jordan Caudill <jcaudill@esi-estech.com>
2017-01-31 14:08:05 -0600

    fixed some unit tests

--- test/user.service.spec.js
+++ test/user.service.spec.js
@@ -7,17 +7,19 @@ describe('userService', function () {
         firstName: 'Abc',
         lastName: 'Lincoln'
     };
+    var serverUrl = 'http://54.202.243.31:9001';
 
     beforeEach(inject(function (_userService_, _$http_, _$q_, _$httpBackend_) {
-        dataService = _userService_;
+        userService = _userService_;
         $http = _$http_;
         $q = _$q_;
         $httpBackend = _$httpBackend_;
+        
     }));
 
     describe('createUser function', function () {
         it('should return data on success', function () {
-            $httpBackend.whenPOST('/users').respond(200, exampleData);
+            $httpBackend.whenPOST(serverUrl + '/users').respond(200, exampleData);
             userService.createUser(exampleData).then(function (res) {
                 expect(res).toEqual(exampleData);
             });

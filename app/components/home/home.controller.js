365224baa4d8b4a11ceed0c6158cf4903c5680d6
Duncan Gallagher <duncang42@gmail.com>
2017-02-18 17:48:23 -0600

    Home Progress

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -5,21 +5,29 @@
         .module('home')
         .controller('HomeController', HomeController);
 
-    // HomeController.$inject = [];
+    HomeController.$inject = ['$localStorage'];
 
-    function HomeController() {
+    function HomeController($localStorage) {
         // Attach functions to the controller here.
-		var vm = this;
-        
-		// Any logic that needs to run when the controller loads should be placed here.
+        var vm = this;
+
+        // Any logic that needs to run when the controller loads should be placed here.
         vm.sorts = [
-          "Last Created",
-          "First Created",
-          "Last Modified",
-          "Name"
-      ];
-        
-		// Define functions here.
-		
+            "Last Created",
+            "First Created",
+            "Last Modified",
+            "Name"
+        ];
+
+        vm.name = $localStorage.tefl.firstName;
+        // Remove later
+        console.log($localStorage.tefl);
+
+        // 
+
+
+
+        // Define functions here.
+
     }
 })();

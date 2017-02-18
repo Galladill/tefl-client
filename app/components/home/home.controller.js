6f2ebf888ba74939f4c11e4269deb2fc67210eb6
Duncan Gallagher <duncang42@gmail.com>
2017-02-18 17:49:02 -0600

    More home progress

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -5,29 +5,50 @@
         .module('home')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$localStorage'];
+    HomeController.$inject = ['$localStorage', 'lessonService', '$q', '$mdToast', 'authService', 'userService'];
 
-    function HomeController($localStorage) {
+    function HomeController($localStorage, lessonService, authService) {
         // Attach functions to the controller here.
         var vm = this;
+        vm.editLesson = _editLesson;
+        vm.deleteLesson = _deleteLesson;
+        vm.createLesson = _createLesson;
+        vm.logout = HomeController.logout;
 
         // Any logic that needs to run when the controller loads should be placed here.
         vm.sorts = [
-            "Last Created",
-            "First Created",
-            "Last Modified",
-            "Name"
+            "Title",
+            "Duration",
+            "Level"
         ];
+        lessonService.getLessons().then(function(lessons, err){
+            vm.lessons = lessons;
+            console.log(lessons);
+        });
+
 
         vm.name = $localStorage.tefl.firstName;
+
         // Remove later
         console.log($localStorage.tefl);
 
-        // 
+        // Get all user's lessons from back-end
+        vm.userLessons = {};
 
+       
 
-
+        
         // Define functions here.
+        function _editLesson() {
+
+        }
+        function _deleteLesson() {
+
+        }
+        function _createLesson() {
+
+        }
+        
 
     }
 })();
